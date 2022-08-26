3f9f417f0220f25be85838856f9133dcedb86322
VladimirJG <vladimi85@gmail.com>
2022-08-26 12:59:09 +0300

    Ready-made algorithm using exceptions and fractions

--- AstonTest/src/Test1Number/Norm.java
+++ AstonTest/src/Test1Number/Norm.java
@@ -1,30 +1,25 @@
 package Test1Number;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Norm {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Go");
-        try {
-            double nextDouble = scanner.nextDouble();
-        } catch (IllegalArgumentException e) {
-            System.out.print("JJJJJJJJ");
-        } finally {
+        System.out.println("Введите число");
 
-            double nextDouble = scanner.nextDouble();
+        try {
+            var nextDouble = scanner.nextDouble();
             if (nextDouble % 1 != 0) {
                 System.out.println("Nu eba");
             } else if (nextDouble > 7) {
                 System.out.println("Hi");
             } else if (nextDouble <= 7) {
                 System.out.println("Number " + nextDouble + " is wrong");
-            } else if (scanner.hasNext()) {
-                System.out.print("Brrr");
             }
-
-
+        } catch (InputMismatchException e) {
+            System.out.print("JJJJJJJJ");
         }
     }
 }
--- AstonTest/src/Test1Number/tryCatch.java
+++ AstonTest/src/Test1Number/tryCatch.java
@@ -1,4 +1,14 @@
 package Test1Number;
 
 public class tryCatch {
+    public static void main(String[] args) {
+        try  {
+            int a = 0;
+            int b = 0;
+            int c = a/b;
+            System.out.println(c);
+        }catch (ArithmeticException e){
+            System.out.println("Barmaley");
+        }
+    }
 }
