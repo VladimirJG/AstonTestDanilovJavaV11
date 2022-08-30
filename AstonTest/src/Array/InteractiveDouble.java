59620a50e60b75253daddd139f266cc0bdb29f01
VladimirJG <vladimi85@gmail.com>
2022-08-30 11:54:48 +0300

    Finished

--- AstonTest/src/Array/InteractiveDouble.java
+++ AstonTest/src/Array/InteractiveDouble.java
@@ -2,6 +2,7 @@ package Array;
 
 import java.util.Scanner;
 import java.util.Locale;
+import java.util.InputMismatchException;
 
 public class InteractiveDouble {
     public static void main(String[] args) {
@@ -11,17 +12,20 @@ public class InteractiveDouble {
         int sizeArray = input.nextInt();
         int array[] = new int[sizeArray];
         System.out.println("Введите элементы массива");
-
-        for (int i = 0; i < sizeArray; i++) {
-            array[i] = (int) input.nextDouble();
-        }
-        System.out.println("Введенные элементы кратные '3'");
-        for (int num : array) {
-            if (num % 3 == 0) {
-                System.out.println(num);
+        try {
+            for (int i = 0; i < sizeArray; i++) {
+                array[i] = (int) input.nextDouble();
             }
+            System.out.println("Введенные элементы кратные '3'");
+
+            for (int num : array) {
+                if (num % 3 == 0) {
+                    System.out.println(num);
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Некоторые из введенных значений не соответствует условиям ввода");
         }
     }
 }
-
 
--- AstonTest/src/Array/NotInteractive.java
+++ AstonTest/src/Array/NotInteractive.java
@@ -2,7 +2,6 @@ package Array;
 
 public class NotInteractive {
     public static void main(String[] args) {
-
         int[] array = {3, 5, 12, 21, 22, 26, 30, 38, 54, 112, 321, 1132};
 
         for (int num : array) {
