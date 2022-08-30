452c35122c34702b089e47c443a0fac5a41cc777
VladimirJG <vladimi85@gmail.com>
2022-08-30 09:15:24 +0300

    Task 3 without double

---
+++ AstonTest/src/Array/Interactive.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.*;
+
+public class Interactive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите размер числового массива");
+        int sizeArray = input.nextInt();
+        int array[] = new int[sizeArray];
+        System.out.println("Введите элементы массива");
+
+        for (int i = 0; i < sizeArray; i++) {
+            array[i] = input.nextInt();
+        }
+        System.out.println("Введенные элементы кратные '3'");
+        for (int num : array) {
+            if (num % 3 == 0) {
+                System.out.println(num);
+            }
+        }
+    }
+}
+
+
